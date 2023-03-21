0391e0b878412c0b97c347b7b7e183e6f8ac8b00
Shogo Takata <peshogo+github.com@gmail.com>
2023-03-22 00:01:30 +0900

    initial

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      curl-http3-overlay =
+        (prev: final: {
+          curl-http3 = prev.curl.override {
+            http3Support = true;
+            openssl = prev.quictls;
+          };
+        });
+    in
+    { overlays.default = curl-http3-overlay; } //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ curl-http3-overlay ]; };
+      in
+      {
+        packages.default = pkgs.curl-http3;
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
